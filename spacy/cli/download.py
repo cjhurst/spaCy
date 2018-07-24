115fc21c430b45c46b4d502a69d0a19d89758b3f
Cory <coryhurst83@gmail.com>
2018-07-24 09:20:21 -0400

    Consolidate 'silent' logic. Abstract out the subprocess call.

--- spacy/cli/download.py
+++ spacy/cli/download.py
@@ -6,12 +6,13 @@ import requests
 import os
 import subprocess
 import sys
+import pkg_resources
 
 from ._messages import Messages
 from .link import link
-from ..util import prints, get_package_path
+from ..util import prints, get_package_path, get_data_path, get_model_meta, is_package
 from .. import about
-
+from ..compat import path2str
 
 @plac.annotations(
     model=("model to download, shortcut or name)", "positional", None, str),
@@ -22,19 +23,16 @@ from .. import about
     pip_args=("additional arguments to be passed to `pip install` when "
               "installing the model")
 )
-
-def download(model, direct=False,  silent = False, *pip_args):
+def download(model, direct=False, silent=False, *pip_args):
     """
     Download compatible model from default download path using pip. Model
     can be shortcut, model name or, if --direct flag is set, full model name
     with version.
     """
     if direct:
+        dl = download_model('{m}/{m}.tar.gz#egg={m}'.format(m=model), pip_args, silent)
         if silent:
-            dl = download_model('{m}/{m}.tar.gz#egg={m}'.format(m=model), pip_args, silent)
             return dl
-        else:
-            download_model('{m}/{m}.tar.gz#egg={m}'.format(m=model), pip_args)
 
     else:
         shortcuts = get_json(about.__shortcuts__, "available shortcuts")
@@ -42,16 +40,14 @@ def download(model, direct=False,  silent = False, *pip_args):
         compatibility = get_compatibility()
         version = get_version(model_name, compatibility)
 
+        dl = download_model('{m}-{v}/{m}-{v}.tar.gz#egg={m}=={v}'
+                            .format(m=model_name, v=version), pip_args, silent)
         if silent:
-            dl = download_model('{m}-{v}/{m}-{v}.tar.gz#egg={m}=={v}'
-                                .format(m=model_name, v=version), pip_args, silent)
             return dl
-        else:
-            dl = download_model('{m}-{v}/{m}-{v}.tar.gz#egg={m}=={v}'
-                                .format(m=model_name, v=version), pip_args)
 
         if dl.returncode != 0:  # if download subprocess doesn't return 0, exit
             sys.exit(dl.returncode)
+
         try:
             # Get package path here because link uses
             # pip.get_installed_distributions() to check if model is a
@@ -67,6 +63,58 @@ def download(model, direct=False,  silent = False, *pip_args):
             prints(Messages.M001.format(name=model_name), title=Messages.M002)
             return dl
         return dl
+
+
+def run_in_shell(cmd, silent=False):
+    if silent:
+        print(silent)
+        # If silent we return Popen object with open pipes
+        dl = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ.copy())
+        print(dl)
+        # wait for the process to finish so it has a return code.
+        dl.wait()
+        return dl
+    else:
+        # If not silent, return Popen object but don't redirect the pipes
+        dl = subprocess.Popen(cmd, env=os.environ.copy(), stdout=None, stderr=None)
+        # wait for the process to finish so it has a return code.
+        dl.wait()
+        return dl
+
+
+def get_pkg_for_link(name):
+    shortcut_path = get_data_path() / name  # path to shortcut link
+    meta = get_model_meta(shortcut_path)
+    model_pkg_name = '{}_{}'.format(meta['lang'], meta['name']).replace('_', '-')
+    shortcut_dest = shortcut_path.resolve().parent
+    dist = [d for d in pkg_resources.working_set
+            if d.module_path == path2str(shortcut_dest)  # in same root directory
+            and d.project_name == model_pkg_name]        # package name matches
+    if dist:
+        return dist[0].project_name.replace('-', '_')
+
+
+def uninstall_model(model_name, silent=False):
+    cmd = [sys.executable, '-m', 'pip', 'uninstall']
+    if silent:
+        cmd.append('-y')
+    cmd.append(model_name)
+    return run_in_shell(cmd, silent)
+
+
+def uninstall(name, silent=False):
+
+    if is_package(name):  # great, name is an installed package
+        return uninstall_model(name, silent)
+
+    link_path = get_data_path() / name
+
+    print(link_path)
+
+    if link_path.exists():  # name is a shortcut link
+        pkg_name = get_pkg_for_link(name)
+        if pkg_name:  # great, link maps to installed package
+            return uninstall_model(pkg_name, silent)
 
 
 def get_json(url, desc):
@@ -96,23 +144,10 @@ def get_version(model, comp):
     return comp[model][0]
 
 
-def download_model(filename, user_pip_args=None, silent= None):
+def download_model(filename, user_pip_args=None, silent=False):
     download_url = about.__download_url__ + '/' + filename
     pip_args = ['--no-cache-dir', '--no-deps']
     if user_pip_args:
         pip_args.extend(user_pip_args)
     cmd = [sys.executable, '-m', 'pip', 'install'] + pip_args + [download_url]
-
-    if silent:
-        # If silent we return Popen object with open pipes
-        dl = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ.copy())
-        # wait for the process to finish so it has a return code.
-        dl.wait()
-        return dl
-
-    else:
-        # If not silent, return Popen object but don't redirect the pipes
-        dl = subprocess.Popen(cmd, env=os.environ.copy(), stdout=None, stderr=None)
-        # wait for the process to finish so it has a return code.
-        dl.wait()
-        return dl
+    return run_in_shell(cmd, silent)
